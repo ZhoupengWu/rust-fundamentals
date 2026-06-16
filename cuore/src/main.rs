b8e7d03e22d9729e98c30f62db33936304fb4949
吴洲鹏 <182263202+ZhoupengWu@users.noreply.github.com>
2026-06-16 10:44:41 +0000

    Cuore provvisorio

--- cuore/src/main.rs
+++ cuore/src/main.rs
@@ -2,23 +2,23 @@ fn main() {
     let middle = 25;
     let top_left = middle - 12;
     let top_right = middle + 12;
-    let mut counter = 0;
 
-    for x in 0..25 {
-        for y in 0..50 {
-            if (x == 15) && (y == top_left || y == top_right) {
-                print!("*");
-
-                continue;
-            }
-
+    for x in 0..=35 {
+        for y in 0..=50 {
             if x < 13 && (y == top_left - x || y == top_left + x || y == top_right - x || y == top_right + x) {
                 print!("*");
-
-                continue;
+            }
+            else if (x >= 13 && x < 20) && (y == x - 13 + 1 || y == 50 - x + 13 - 1) {
+                print!("*");
+            }
+            else if (x >= 20 && x < 27) && (y == x - 13 + 2 || y == 50 - x + 13 - 2) {
+                print!("*");
+            }
+            else if (x >= 27) && (y == x - 13 + 3 || y == 50 - x + 13 - 3) {
+                print!("*");
             }
             else {
-                print!("#");
+                print!(" ");
             }
         }
 
