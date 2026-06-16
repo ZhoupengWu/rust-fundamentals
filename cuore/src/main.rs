3168b9b1a48f7eaeaf6fd8b5468794e04121cf52
吴洲鹏 <182263202+ZhoupengWu@users.noreply.github.com>
2026-06-16 17:42:29 +0000

    Cuore riempito work in progress

--- cuore/src/main.rs
+++ cuore/src/main.rs
@@ -8,7 +8,10 @@ fn main() {
         let base_end = 50 - x + 13;
 
         for y in 0..=50 {
-            if (x < 13 && (y == top_left - x || y == top_right + x)) || (x < 12 && (y + 1 == top_left + x + 2 || y - 1 == top_right - x - 2)) {
+            if x < 12 && (y + 1 == top_left + x + 2 || y - 1 == top_right - x - 2) {
+                print!("*");
+            }
+            else if x < 13 && (y == top_left - x || y == top_right + x) {
                 print!("*");
             }
             else if (x >= 13 && x < 18) && (y == base_start + 1 || y == base_end - 1) {
